ab019cc4736fea57cea967ebd006de1ad6173bb2
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-04 23:15:23 +0600

    create model and form

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    list_display = ['username', 'phone', 'email']
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'myapp/home.html')
+def display_data(request):
+    return render(request, 'myapp/display_data.html')
